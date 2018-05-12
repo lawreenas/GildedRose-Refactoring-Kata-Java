e8c02a3d8c92b319b7921ba9e466f67f04c42d50
Laurynas Remeika <laurynas@inventi.lt>
2018-05-12 10:23:53 +0300

    refactor[java8]: use streams navigating an array of items

---
+++ src/main/java/com/gildedrose/ItemType.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+/**
+ * @author L.Remeika
+ */
+public enum ItemType {
+
+    AGED_BRIE("Aged Brie"),
+    BACKSTAGE_PASS("Backstage passes to a TAFKAL80ETC concert"),
+    SULFURAS("Sulfuras, Hand of Ragnaros");
+
+    ItemType(String name) {
+        this.name = name;
+    }
+
+    public final String name;
+
+}
