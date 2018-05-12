8dcb7b4f93055ead5d67eb27ca16196259c20528
Laurynas Remeika <laurynas@inventi.lt>
2018-05-12 09:17:12 +0300

    refactor: extract sell in logic to private method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 class GildedRose {
     Item[] items;
 
@@ -8,7 +10,11 @@ class GildedRose {
     }
 
     public void updateQuality() {
+
         for (int i = 0; i < items.length; i++) {
+
+            items[i] = updateItemSellIn(items[i]);
+
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
@@ -36,10 +42,6 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -59,4 +61,11 @@ class GildedRose {
             }
         }
     }
+
+    private Item updateItemSellIn(Item item) {
+        if ("Sulfuras, Hand of Ragnaros".equals(item.name)) return item;
+        item.sellIn -= 1;
+        return item;
+    }
+
 }
